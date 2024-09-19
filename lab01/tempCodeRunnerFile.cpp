2c7ae0ac37f7a36dcc9ae001aff42f91ee352ca1
ahilacondo <ahilacondo@unsa.edu.pe>
2024-09-19 11:11:24 -0500

    se agrego el uso de arreglos

--- lab01/tempCodeRunnerFile.cpp
+++ lab01/tempCodeRunnerFile.cpp
@@ -9,30 +9,19 @@ using namespace std;
 int esAnstrong(int n); //Funcion que nos dice si un numero es cubo perfecto
 inline int cubo(int n); //Funcion que nos da el cubo de un numero
 bool esNumValido(const string &str); //Funcion que nos dice si un string es un numero valido
-void obtenerDigitos(int n, int digitos[], int &tam); //Funcion que nos da los digitos de un numero
 
 inline int cubo(int n){
     return n * n * n;
 }
 
-void obtenerDigitos(int n, int digitos[], int &tam){
-    tam = 0;
-    while (n > 0){
-        digitos[tam++] = n % 10;
-        n /= 10;
-    }
-}
-
 int esAnstrong(int n){
-    int tam;
-    int digitos[10];
-    obtenerDigitos(n, digitos, tam);
-
     int suma = 0;
-    while(tam > 0){
-        suma += cubo(digitos[--tam]);
+    int aux = n;
+    while(aux > 0){
+        suma += cubo(aux % 10);
+        aux /= 10;
     }
-    return suma == n;
+    return suma == n; 
 }
 
 bool esNumValido(const string &str){
@@ -54,7 +43,7 @@ int main(){
         if(esNumValido(str)){
             stringstream ss(str);
             ss >> num;
-
+            
             if(esAnstrong(num))
                 cout << "El numero ingresado es cubo perfecto" << endl;
             else
