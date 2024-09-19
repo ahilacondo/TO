1b154ff7090d15f9f42e1837994d9b1b15c483d8
ahilacondo <ahilacondo@unsa.edu.pe>
2024-09-19 09:38:42 -0500

    Para que no salga errores

--- lab01/ejer01.cpp
+++ lab01/ejer01.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <cctype>
+#include <limits>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
 int esAnstrong(int n); //Funcion que nos dice si un numero es cubo perfecto
 inline int cubo(int n); //Funcion que nos da el cubo de un numero
+bool esNumValido(const string &str); //Funcion que nos dice si un string es un numero valido
 
 inline int cubo(int n){
     return n * n * n;
@@ -20,6 +24,13 @@ int esAnstrong(int n){
     return suma == n; 
 }
 
+bool esNumValido(const string &str){
+    for(char c: str){
+        if(!isdigit(c))
+            return false;
+    }
+    return true;
+}
 int main(){
     char seguir = 's';
     while (tolower(seguir) == 's'){
