a846e1b3a1e7c25213197379a0628c2951986223
ahilacondo <ahilacondo@unsa.edu.pe>
2024-09-20 22:02:37 -0500

    ya agregamos las demas funciones necesarias, como contabilizar el grado e imprimir datos

--- lab01/ejer02.cpp
+++ lab01/ejer02.cpp
@@ -16,6 +16,9 @@ struct persona{
 
 int generarAleatorio(int min, int max); //Funcion que genera numeros aleatorios
 void imprimirPersonas(persona personas[], int n);
+void imprimirDatos(); //Esta funcion va imprimir los datos que requiere el ejercicio
+int edadMayor(persona persona[], int n);
+
 
 int generarAleatorio(int min, int max){
     return rand() % (max - min + 1) + min;
@@ -27,10 +30,30 @@ void imprimirPersonas(persona personas[], int n){
     }
 }
 
+int edadMayor(persona personas[], int n){
+    int mayor = personas[0].edad;
+    for(int i = 1; i < n; i++){
+        if(mayor < personas[i].edad)
+            mayor = personas[i].edad;
+    }
+
+    return mayor;
+}
+
+void imprimirDatos(persona personas[], int n, int cantPrimaria, int cantSecundaria, int cantSuperior){
+    cout << "Cantidad de personas con grado de primaria: " << cantPrimaria << endl;
+    cout << "Cantidad de personas con grado de secundaria: " << cantSecundaria << endl;
+    cout << "Cantidad de personas con grado de superior: " << cantSuperior << endl;
+    cout << "Edad de la persona mayor: " << edadMayor(personas, n) << endl;
+}
+
 int main(){
     int n = 10; //numero de personas
     int edadMin = 15;
     int edadMax = 80;
+    int cantPrimaria = 0;
+    int cantSecundaria = 0;
+    int cantSuperior = 0;
     srand(time(0));
     
     persona personas[n];
@@ -42,16 +65,20 @@ int main(){
         switch(g){
             case PRIMARIA:
                 personas[i].grado = "Primaria";
+                cantPrimaria++;
                 break;
             case SECUNDARIA:
                 personas[i].grado = "Secundaria";
+                cantSecundaria++;
                 break;
             case SUPERIOR:
                 personas[i].grado = "Superior";
+                cantSuperior++;
                 break;
         }
     }
     
     imprimirPersonas(personas, n);
+    imprimirDatos(personas, n, cantPrimaria, cantSecundaria, cantSuperior);
     return 0;
 }
